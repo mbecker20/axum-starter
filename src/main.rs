69c90878efea896979aea30098f0bb98d593ef77
mbecker20 <becker.maxh@gmail.com>
2024-07-04 21:24:50 -0700

    add env

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
-use std::{net::SocketAddr, str::FromStr};
+#[macro_use]
+extern crate tracing;
+
+use std::{net::SocketAddr, str::FromStr, sync::OnceLock};
 
 use anyhow::Context;
 use axum::{routing::get, Router};
+use serde_derive::Deserialize;
 
-#[macro_use]
-extern crate tracing;
+#[derive(Deserialize)]
+struct Env {
+  #[serde(default = "default_port")]
+  port: u16,
+}
+
+fn default_port() -> u16 {
+  80
+}
+
+fn env() -> &'static Env {
+  static ENV: OnceLock<Env> = OnceLock::new();
+  ENV.get_or_init(|| {
+    envy::from_env()
+      .context("failed to parse environment")
+      .unwrap()
+  })
+}
 
 async fn app() -> anyhow::Result<()> {
   let app =
     Router::new().route("/", get(|| async { "hello, world" }));
 
-  let socket_addr = SocketAddr::from_str("0.0.0.0:80")
-    .context("failed to parse socket addr")?;
+  let socket_addr =
+    SocketAddr::from_str(&format!("0.0.0.0:{}", env().port))
+      .context("failed to parse socket addr")?;
 
   let listener = tokio::net::TcpListener::bind(&socket_addr)
     .await
